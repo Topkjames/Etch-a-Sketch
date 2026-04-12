10e193aeb7b23d0ce6bdc5d364d119cce14da51a
Topkjames <topkjames@gmail.com>
2026-04-12 11:45:47 +0100

    Finish the createGrid function and add event listener to the clear button. and also call it at the end to activate the grid when the page loads.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,16 @@ function createGrid(size) {
     square.classList.add("square");
     square.style.width = `${squareSize}px`;
     square.style.height = `${squareSize}px`;
+
+    square.addEventListener("mouseover", () => {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
+
     container.appendChild(square);
   }
 }
+
+createGrid(16);
